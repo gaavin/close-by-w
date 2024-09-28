323c8cc19c64573d81aab211fdbbc3ebb1993b00
Gavin Power <me@gavinpower.dev>
2024-09-28 04:24:54 -0230

    add simple reactivity example

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,24 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal } from "@builder.io/qwik";
 
 export default component$(() => {
+  const clickCount = useSignal(0);
+
   return (
     <div class="flex flex-col">
       <h2>Helloooo...</h2>
       <h3>welcome to the snow clearing app</h3>
-      <button class="cb-button">Let it snow</button>
+      <button
+        class="cb-button"
+        onClick$={() => {
+          clickCount.value++;
+          if (clickCount.value > 3) {
+            alert("it has snowed enough.");
+          }
+        }}
+      >
+        Let it snow
+      </button>
+      <p>snowed {clickCount.value} times</p>
     </div>
   );
 });

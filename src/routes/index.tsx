e18a4d6e86b8202a93a4cb1658dc1f700dc95bac
Gavin Power <me@gavinpower.dev>
2024-09-29 12:12:58 -0230

    ww

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,15 +2,13 @@ import { component$ } from "@builder.io/qwik";
 
 export default component$(() => {
   return (
-    <>
+    <div>
       <h1>
         <h1>Thank you for visiting my website</h1>
-        <br />
         <p>
           Hi, my website is under construction right now, but if you come back
           soon you'll find blog posts, personal projects, and my resume 🐈
         </p>
-        <br />
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
           facilisi.
@@ -21,6 +19,6 @@ export default component$(() => {
         </p>
       </h1>
       <button class="cb-button">Click me</button>
-    </>
+    </div>
   );
 });

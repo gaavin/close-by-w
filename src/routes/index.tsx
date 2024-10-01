5ed6a912fadc62c9d714290374397fdbd3ad95ac
Gavin Power <me@gavinpower.dev>
2024-10-01 00:20:50 -0230

    asdsd

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,8 +3,8 @@ import { component$ } from "@builder.io/qwik";
 export default component$(() => {
   return (
     <>
-      <h1>gavinpower.dev✨</h1>
-      <h2>thank you for visiting my website🤍</h2>
+      <h1>welcome✨</h1>
+      <h2>thank you for visiting my website</h2>
       <br />
       <p>
         hi, this place is under construction right now!

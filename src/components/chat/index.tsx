695cd28eac3f5942dc99840e86993c2b819e9fa0
Gavin Power <me@gavinpower.dev>
2024-10-02 20:20:43 -0230

    asds

--- src/components/chat/index.tsx
+++ src/components/chat/index.tsx
@@ -3,7 +3,14 @@ import { component$, Slot } from "@builder.io/qwik";
 export default component$(() => {
   return (
     <>
-      <div>
+      <div
+        style={{
+          height: "20rem",
+          width: "20rem",
+          background:
+            "rgba(var(--dough-r), var(--dough-g), var(--dough-b), 0.1)",
+        }}
+      >
         <p>Chat!</p>
         <Slot />
       </div>

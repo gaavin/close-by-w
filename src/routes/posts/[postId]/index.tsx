2da4755ed643968da531bcc65f9f599d7fa80406
Gavin Power <me@gavinpower.dev>
2024-10-02 17:32:37 -0230

    asdsaasdas

--- src/routes/posts/[postId]/index.tsx
+++ src/routes/posts/[postId]/index.tsx
@@ -1,9 +1,15 @@
 import { component$ } from "@builder.io/qwik";
+import { routeLoader$ } from "@builder.io/qwik-city";
+import { drizzle } from "drizzle-orm/d1";
+import * as schema from "~/schema";
+
+export const usePost = routeLoader$(async (requestEvent) => {
+  const { D1 } = requestEvent.platform.env;
+  const db = drizzle(D1, { schema });
+  return db.query.posts.findFirst();
+});
 
 export default component$(() => {
-  return (
-    <>
-      <span>nothing here yet</span>
-    </>
-  );
+  const post = usePost();
+  return <>{post.value?.content}</>;
 });
--- src/routes/posts/layout.tsx
+++ src/routes/posts/layout.tsx
@@ -29,20 +29,18 @@ export default component$(() => {
   return (
     <>
       <h1>Posts</h1>
-      <ul>
-        {posts.value.map((post) => (
-          <li key={post.id}>
-            <label>Title</label>
-            <a href={post.id.toString()}>{post.title}</a>
-            <label>Author</label>
-            <li>{post.author.username}</li>
-            <label>Date</label>
-            <li>{post.createdAt.toUTCString()}</li>
-            <label>Last Edited</label>
-            <li>{post.createdAt.toUTCString()}</li>
-          </li>
-        ))}
-      </ul>
+      {posts.value.map((post) => (
+        <a href={post.id.toString()}>
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <h2>
+              {post.author.username} - {post.createdAt.toLocaleDateString()}
+            </h2>
+            <h3>ID (DESCRIPTION){post.id}</h3>
+            <div></div>
+          </div>
+        </a>
+      ))}
 
       <label>Recent Posts</label>
       <ul>

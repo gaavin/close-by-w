a96099b2d82a9d48f5b56c6dd15e82bd3e74dc81
Gavin Power <me@gavinpower.dev>
2024-10-02 16:20:28 -0230

    asds

--- src/routes/posts/layout.tsx
+++ src/routes/posts/layout.tsx
@@ -3,32 +3,36 @@ import { routeLoader$ } from "@builder.io/qwik-city";
 import { drizzle } from "drizzle-orm/d1";
 import * as schema from "~/schema";
 
-export const useUsers = routeLoader$(async (requestEvent) => {
+export const usePostList = routeLoader$(async (requestEvent) => {
   const { D1 } = requestEvent.platform.env;
   const db = drizzle(D1, { schema });
-  const posts = db.query.posts.findMany();
+  const posts = db.query.posts.findMany({
+    columns: {
+      id: true,
+      userId: true,
+      createdAt: true,
+      title: true,
+      description: true,
+    },
+  });
   return posts;
 });
 
 export default component$(() => {
-  const posts = useUsers();
+  const posts = usePostList();
   return (
     <>
       <div>
         <label>Posts</label>
         <ul>
           {posts.value.map((post) => (
-            <li>
-              <ul>
-                <label>Id</label>
-                <li>{post.id}</li>
-                <label>Author</label>
-                <li>{post.userId}</li>
-                <label>Date</label>
-                <li>{post.createdAt.toUTCString()}</li>
-                <label>Last Edited: </label>
-                <li>{post.updatedAt.toUTCString()}</li>
-              </ul>
+            <li key={post.id}>
+              <label>Title</label>
+              <a href={post.id.toString()}>{post.title}</a>
+              <label>Author</label>
+              <li>{post.userId}</li>
+              <label>Date</label>
+              <li>{post.createdAt.toUTCString()}</li>
             </li>
           ))}
         </ul>

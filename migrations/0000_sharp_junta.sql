fc101603f7393788a1951b34bfd4214783bba5c9
Gavin Power <me@gavinpower.dev>
2024-10-02 15:30:38 -0230

    asdas

--- migrations/0000_sharp_junta.sql
+++ migrations/0000_sharp_junta.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `users` (
-	`id` text,
+	`id` integer PRIMARY KEY NOT NULL,
 	`username` text,
 	`created_at` integer DEFAULT (unixepoch() * 1000) NOT NULL,
 	`updated_at` integer DEFAULT (unixepoch() * 1000) NOT NULL
--- src/schema.ts
+++ src/schema.ts
@@ -2,7 +2,7 @@ import { sql } from "drizzle-orm";
 import { text, sqliteTable, integer } from "drizzle-orm/sqlite-core";
 
 export const usersTable = sqliteTable("users", {
-  id: text("id"),
+  id: integer("id").primaryKey(),
   username: text("username"),
   createdAt: integer("created_at", { mode: "timestamp_ms" })
     .notNull()

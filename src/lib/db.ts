df0b7d4f25446536f0721daa308eed6ad5d8c5d1
Gavin Power <me@gavinpower.dev>
2024-10-04 23:02:35 -0230

    fuck

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,8 @@
 import { drizzle } from "drizzle-orm/d1";
-import { getD1 } from "~/compat";
-import * as schema from "~/lib/schema";
 import type { RequestEventLoader } from "@builder.io/qwik-city";
+// TODO: these *have* to be ../ imports to be resolved correctly at buildtime
+import { getD1 } from "../compat";
+import * as schema from "../lib/schema";
 
 export const drizzleFactory = async (
   requestEvent: RequestEventLoader<QwikCityPlatform>
